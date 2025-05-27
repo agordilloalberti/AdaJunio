5592af28087e94d5c77cae65eb9e9831464fd3e4
Alvarogogal <agorsua0610@g.educaand.es>
2025-05-27 19:46:45 +0200

    First Commit

---
+++ src/main/kotlin/com/AdaJunio/AdaJunioApplication.kt
@@ -0,0 +1,11 @@
+package com.AdaJunio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdaJunioApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdaJunioApplication>(*args)
+}
---
+++ src/test/kotlin/com/AdaJunio/AdaJunioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.AdaJunio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdaJunioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
